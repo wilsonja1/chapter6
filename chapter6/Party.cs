05caec987f528eb2f2af1d083eec8a1de5d929fa
Jack Wilson <battlejackmaster@gmail.com>
2018-07-13 18:35:17 -0700

    Add project files.

---
+++ chapter6/Party.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter6
+{
+    class Party
+    {
+        public int NumberOfPeople { get; set; }
+        public bool FancyDecorations { get; set; }
+    }
+}
